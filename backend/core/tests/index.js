8f1179adc489ebed26cc7ae59b047a33f14fc546
aHIPPOz <hippolyte.jeggycheynet@arauderghem.net>
2026-02-11 19:19:16 +0100

    b

--- backend/core/tests/index.js
+++ backend/core/tests/index.js
@@ -1,4 +1,2 @@
-import assert from "assert";
-import { add } from "../build/debug.js";
-assert.strictEqual(add(1, 2), 3);
-console.log("ok");
+import * from "../build/debug.js";
+nexus.console.log("ok");
